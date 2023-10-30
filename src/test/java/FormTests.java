184152f7cdf139edf6667fc750599c6deef7db8b
a.varenyshev <alex.varenyshev@gmail.com>
2023-10-30 19:39:21 +0300

    fixed upload picture. Final

--- src/test/java/FormTests.java
+++ src/test/java/FormTests.java
@@ -1,8 +1,10 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -24,15 +26,33 @@ public class FormTests {
         $("[for='gender-radio-1']").click();
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
-        $("[]").setValue("").pressEnter();
-        $("[]").setValue("").pressEnter();
-        $("[]").setValue("").pressEnter();
-        $("[]").setValue("").pressEnter();
-        $("[]").setValue("").pressEnter();
-        $("[]").setValue("").pressEnter();
-        $("#search").shouldHave(text("https://ru.selenide.org"));
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__month-select").selectOptionByValue("3");
+        $(".react-datepicker__day--011:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("toolsqa.jpg");
+        $("#currentAddress").setValue("Gogol st.");
+        $("#stateCity-label").scrollTo();
+        $("#react-select-3-input").val("Uttar Pradesh").pressEnter();
+        $("#react-select-4-input").val("Agra").pressEnter();
+        $("#submit").pressEnter();
+
+
+        $(".table-responsive").shouldHave(
+                text("Bradd Pitt"),
+                text("brad@pitt.com"),
+                text("Male"),
+                text("1234567890"),
+                text("11 April,1990"),
+                text("Math"),
+                text("Sports"),
+                text("toolsqa.jpg"),
+                text("Gogol st."),
+                text("Uttar Pradesh Agra")
+        );
     }
 }
 
-
+//selectOptionByValue("11")
 
