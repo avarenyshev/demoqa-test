7a85c4bce74bc40dc9d5dafa1efd3aa184f961c3
Alexandr Varenyshev <alex.varenyshev@gmail.com>
2023-11-15 23:45:23 +0300

    prepearng to add pages objects

--- src/test/java/pages/RegistationPage.java
+++ src/test/java/pages/RegistationPage.java
@@ -1,9 +1,47 @@
 package pages;
 
-import static com.codeborne.selenide.Selenide.$;
-public class RegistationPage {
-    public static String = firstNameLocator = "#first_name";
+import com.codeborne.selenide.SelenideElement;
 
-    public void setFirstName() {$(firstNameLocator).sendKeys();
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistationPage {
+    public void openPage() {
+        open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
     }
+
+    private SelenideElement
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastname"),
+            emailUserInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber");
+
+
+
+    public RegistationPage setFirstName(String value) {
+        $(firstNameInput).setValue(value);
+
+        return this;
+    }
+
+    public RegistationPage setLastName(String value) {
+        $(lastNameInput).setValue(value);
+        return this;
+    }
+    public RegistationPage setUserEmail (String value) {
+        $(emailUserInput).setValue(value);
+        return this;
+    }
+    public RegistationPage setGender (String value) {
+        genderWrapper.$(byText("value")).click();
+        return this;
+    }
+    public RegistationPage setUserNuber (String value) {
+        $(userNumberInput).setValue(value);
+        return this;
 }
---
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -0,0 +1,60 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.RegistationPage;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationWithPageObjectsTests extends TestBase {
+
+    RegistationPage registationPage = new RegistationPage();
+
+    @Test
+    void fillFormTests() {
+        registationPage.openPage()
+                .setFirstName("Bradd")
+                .setLastName("Pitt")
+                .setUserEmail("brad@pitt.com")
+                .setGender("Male")
+                .setUserNuber("1234567890")
+                .setBirthday("11", "April", "1990");
+        .setSubjeects("Math");
+        .sethobbies("Sports");
+        .setPicture("123.jpg");
+        .setAdress("Gogol st.");
+        .setState("Uttar Pradesh");
+        .setCity("Agra");
+        $("#submit").pressEnter();
+//check
+//        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        registrationPage.checkFormAppears()
+
+        registrationPage.checkResult("Full name", "Bradd Pitt");
+    }
+}
+
+//        $(".table-responsive").$(byText("Student Name"))
+//                .parent().shouldHave(text("Bradd Pitt"));
+//        $(".table-responsive").$(byText("Student Email"))
+//                .parent().shouldHave(text("brad@pitt.com"));
+//        $(".table-responsive").$(byText("Gender"))
+//                .parent().shouldHave(text("Male"));
+//        $(".table-responsive").$(byText("Mobile"))
+//                .parent().shouldHave(text("1234567890"));
+//        $(".table-responsive").$(byText("Date of Birth"))
+//                .parent().shouldHave(text("11 April,1990"));
+//        $(".table-responsive").$(byText("Subjects"))
+//                .parent().shouldHave(text("Math"));
+//        $(".table-responsive").$(byText("Hobbies"))
+//                .parent().shouldHave(text("Sports"));
+//        $(".table-responsive").$(byText("Picture"))
+//                .parent().shouldHave(text("123.jpg"));
+//        $(".table-responsive").$(byText("Address"))
+//                .parent().shouldHave(text("Gogol st."));
+//        $(".table-responsive").$(byText("State and City"))
+//                .parent().shouldHave(text("Uttar Pradesh Agra"));
+//        $("#closeLargeModal").click();
