11b2e359710d109c77306918ac9b58ee9810695c
Alexandr Varenyshev <alex.varenyshev@gmail.com>
2023-12-24 21:06:28 +0300

    corrected TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,14 +12,10 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browser = System.getProperty("browser", "safari");
-        Configuration.browser = System.getProperty("browser", "mozila");
         Configuration.browserSize = System.getProperty("size","1920x1080");
-        Configuration.browserSize = System.getProperty("size","1280x1024");
-        Configuration.browserSize = System.getProperty("size","1024x960");
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = false;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -29,12 +25,5 @@ public class TestBase {
         ));
         Configuration.browserCapabilities = capabilities;
 
-//    static void beforeAll() {
-//        Configuration.remote = System.getProperty("selenoid", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-//        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-//        Configuration.browser = System.getProperty("browser", "chrome");
-//        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-//        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-//    }
     }
 }
